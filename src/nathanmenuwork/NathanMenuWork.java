07987b310ed34be3a208c97736d2f6e8eba06eae
344_13 <344_13@DESKTOP-501ON78.mshome.net>
2025-06-06 17:05:58 +0700

    T20

--- src/nathanmenuwork/NathanMenuWork.java
+++ src/nathanmenuwork/NathanMenuWork.java
@@ -16,9 +16,11 @@ public class NathanMenuWork {
             System.out.println("#############################");
             System.out.println("#########   Menu   ##########");
             System.out.println("#############################");
-            System.out.println("#### 1) Plus ###");
-            System.out.println("#### 2) Minus ###");
-            System.out.println("#### 3) Exit ###");
+            System.out.println("#### 1) Plus");
+            System.out.println("#### 2) Minus");
+            System.out.println("#### 3) Multiply");
+            System.out.println("#### 4) Exit");
+            System.out.println("#### 5) Multiplication Table");
             System.out.print("Enter your menu : ");
             choice = scan.nextInt();
 
@@ -30,14 +32,20 @@ public class NathanMenuWork {
                     minus(scan);
                     break;
                 case 3:
+                    multiply(scan);
+                    break;
+                case 4:
                     System.out.println("Exiting program...");
+                    break;
+                case 5:
+                    showMultiplicationTable(scan);
                     break;
                 default:
                     System.out.println("Invalid choice! Please try again.");
             }
 
             System.out.println(); // Line spacing
-        } while (choice != 3);
+        } while (choice != 4);
     }
 
     public static void plus(Scanner scan) {
@@ -58,5 +66,25 @@ public class NathanMenuWork {
         num2 = scan.nextInt();
         result = num1 - num2;
         System.out.println("Result: " + result);
+    }
+
+    public static void multiply(Scanner scan) {
+        int num1, num2, result;
+        System.out.print("Enter first number: ");
+        num1 = scan.nextInt();
+        System.out.print("Enter second number: ");
+        num2 = scan.nextInt();
+        result = num1 * num2;
+        System.out.println("Result: " + result);
+    }
+
+    public static void showMultiplicationTable(Scanner scan) {
+        System.out.print("Enter a number for multiplication table: ");
+        int number = scan.nextInt();
+
+        System.out.println("Multiplication Table for " + number + ":");
+        for (int i = 1; i <= 12; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
     }
 }
